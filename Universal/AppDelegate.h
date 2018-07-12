5e22d438c5bfabaff4a2211ad3eda7d81456aa52
liuziyang <lzy110314@163.com>
2018-07-12 08:59:10 +0800

    Initial Commit

---
+++ Universal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Universal
+//
+//  Created by zl on 2018/7/12.
+//  Copyright © 2018年 Hangzhou Xiangyi Investment Management Co. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Universal/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Universal
+//
+//  Created by zl on 2018/7/12.
+//  Copyright © 2018年 Hangzhou Xiangyi Investment Management Co. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
